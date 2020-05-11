9db2708286f2d155f15781e4dd3e8d6e31c42501
MrGwei <guoweimail@126.com>
2020-05-11 17:24:08 +0800

    添加Eureka客户端

--- springcloud/README.md
+++ springcloud/README.md
@@ -1,1 +1,7 @@
-- eureka-server
+学习Spring Cloud框架的总结，使用的是最新的Hoxton版本。主要包括Eureka、Ribbon、Hystrix、Feign、Zuul、Gateway、Security、Bus、OpenFeign等核心组件用法进行详细介绍。
+
+参考：https://blog.csdn.net/ThinkWon/article/details/103715146
+
+01.
+- eureka-server 服务注册中心
+- eureka-client eureka客户端
---
+++ springcloud/eureka-client/src/test/java/com/wfx/projects/springcloud/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfx.projects.springcloud.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
